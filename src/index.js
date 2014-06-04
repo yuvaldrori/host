1ffcefba846759543c2b36e2f30aae3ab99ed4b0
Yuval Drori <yuval.drori@gmail.com>
2014-06-04 18:05:00 +0000

    utils + event

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 'use strict';
 
+var randomGender = function () {
+  return Math.random() > 0.5 ? 'F' : 'M';
+};
+var demoList = function (n) {
+  var array = [];
+  for (var i = 0; i < n; i += 1) {
+    array.push({id: i, name: i, gender: randomGender()});
+  }
+  return array;
+};
+var countGirls = function (array) {
+  return array.reduce(function (p, c) {
+    return c.gender === 'F' ? p + 1 : p;
+  }, 0);
+};
+var ratio = function (girls, total) {
+  return girls * 100 / total;
+};
+
 function Kids (array) {
   this.a = [];
   this.total = 0;
   this.girls = 0;
   this.boys = 0;
   this.ratio = 0;
-
-  var randomGender = function () {
-    return Math.random() > 0.5 ? 'F' : 'M';
-  };
-  var demoList = function (n) {
-    var array = [];
-    for (var i = 0; i < n; i += 1) {
-      array.push({id: i, name: i, gender: randomGender()});
-    }
-    return array;
-  };
-  var countGirls = function (array) {
-    return array.reduce(function (p, c) {
-      return c.gender === 'F' ? p + 1 : p;
-    }, 0);
-  };
-  var ratio = function (girls, total) {
-    return girls * 100 / total;
-  };
 
   this.update = function (array) {
     this.a = array;
@@ -66,10 +66,38 @@ function Freq (n) {
   this.update = function (kida, kidb) {
     this.a[kida][kidb] += 1;
   };
-  this.getFreq = function (kida, kidb) {
-    return this.a[kida][kidb] + this.a[kidb][kida];
+  this.getFreq = function (kida, kids) {
+    var score = 0;
+    var a = [];
+    a = kids instanceof Array ? kids : a.push(kids);
+    for (var i = 0; i < a.length; i += 1) {
+      score += this.a[kida][a[i]] + this.a[a[i]][kida];
+    }
+    return score;
   };
   this.debug = function () {
     return this.a;
   };
 }
+
+function Event () {
+  this.a = [];
+  this.total = 0;
+  this.girls = 0;
+  this.boys = 0;
+  this.ratio = 0;
+
+  this.update = function (kid) {
+    this.a.push(kid);
+    this.total = this.a.length;
+    this.girls = countGirls(this.a);
+    this.boys = this.total = this.girls;
+    this.ration = ratio (this.girls, this.total);
+  };
+  this.score = function (kid, freq) {
+    return freq.getFreq (kid, freq);
+  };
+}
+
+var kids = new Kids();
+var freq = new Freq(kids.total);
