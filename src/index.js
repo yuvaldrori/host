47fd49f1cdd3c841162af19e6a36e490e7f34351
Yuval Drori <yuval.drori@gmail.com>
2014-06-04 17:14:53 +0000

    wip oop

--- src/index.js
+++ src/index.js
@@ -1,89 +1,75 @@
 'use strict';
 
-//frequency of kids in the same group
-var freq = [];
-//array of kid with name and gender
-var kids = [];
-//how many kids in a group
-var kidsInGroup = 0;
-//true - group shuffling, false - change hosts in single group
-var shuffle = true;
-//results written here
-var results = [];
-//events
-var events = 0;
-var groups = 0;
-var mod = 0;
+function Kids (array) {
+  this.a = [];
+  this.total = 0;
+  this.girls = 0;
+  this.boys = 0;
+  this.ratio = 0;
 
-var genRandomSex = function () {
-  if (Math.random() > 0.5) {
-    return 'F';
-  } else {
-    return 'M';
-  }
-};
-
-var initTestGroup = function (n) {
-  while (kids.length) {
-    kids.pop();
-  }
-  for (var i = 0; i < n; i += 1) {
-    kids.push({id: i, name: i, sex: genRandomSex()});
-  }
-};
-
-var isGirl = function (index) {
-  if (kids[index].sex === 'F') {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-var girls = function () {
-  return kids.reduce(function(p, c) {
-    if (c.sex === 'F') {
-      return p + 1;
-    } else {
-      return p;
+  var randomGender = function () {
+    return Math.random() > 0.5 ? 'F' : 'M';
+  };
+  var demoList = function (n) {
+    var array = [];
+    for (var i = 0; i < n; i += 1) {
+      array.push({id: i, name: i, gender: randomGender()});
     }
-  }, 0);
-};
+    return array;
+  };
+  var countGirls = function (array) {
+    return array.reduce(function (p, c) {
+      return c.gender === 'F' ? p + 1 : p;
+    }, 0);
+  };
+  var ratio = function (girls, total) {
+    return girls * 100 / total;
+  };
 
-var boys = function () {
-  return kids.length - girls();
-};
+  this.update = function (array) {
+    this.a = array;
+    this.total = this.a.length;
+    this.girls = countGirls(array);
+    this.boys = this.total - this.girls;
+    this.ratio = ratio(this.girls, this.total);
+  };
 
-var initFreqArray = function () {
-  var nKids = kids.length;
-  for (var i = 0; i < nKids; i += 1) {
-    freq[i] = [];
-    for (var j = 0; j < nKids; j += 1) {
-      freq[i].push(0);
-    }
-  }
-};
+  this.update(array === undefined ? demoList(35) : array);
 
-var initResultsWithHosts = function () {
-  while (results.length) {
-    results.pop();
-  }
-  var index = 0;
-  groups = Math.floor(kids.length / kidsInGroup);
-  for (var i = 0; i < events; i += 1) {
-    results.push([]);
-    for (var j = 0; j < groups; j += 1) {
-      results[i].push([kids[index]]);
-      index += 1;
-      if (index === kids.length) {
-        index = 0;
+  this.debug = function () {
+    return {
+      a: this.a,
+      total: this.total,
+      girsl: this.girls,
+      boys: this.boys,
+      ratio: this.ratio
+    };
+  };
+}
+
+function Freq (n) {
+  this.a = [];
+
+  var init = function (n) {
+    var a = [];
+    for (var i = 0; i < n; i += 1) {
+      a[i] = [];
+      for (var j = 0; j < n; j += 1) {
+        a[i].push(0);
       }
     }
-  }
-};
+    return a;
+  };
 
-var updateFreq = function (kidIndex, groupIdx) {
-  for (var i = 0; i < results[groupIdx].length; i += 1) {
-    freq[kidIndex][results[groupIdx][i].id] += 1;
-  }
-};
+  this.a = init(n);
+
+  this.update = function (kida, kidb) {
+    this.a[kida][kidb] += 1;
+  };
+  this.getFreq = function (kida, kidb) {
+    return this.a[kida][kidb] + this.a[kidb][kida];
+  };
+  this.debug = function () {
+    return this.a;
+  };
+}
