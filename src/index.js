c8afb3a6550e4d5945a5fe29c977b0ccd80c3d97
Yuval Drori <yuval.drori@gmail.com>
2014-07-12 23:53:05 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ function HOST () {
   var frequency = [];
 
   // building blocks
-
   function Person (id, name, gender) {
     this.id = id;
     this.name = name;
@@ -29,5 +28,91 @@ function HOST () {
   function Shift () {
     this.groups = [];
   }
+
+  // utils
+  function arrayMinValue (array) {
+    return array.reduce(function(p, c){
+      return p > c ? c : p;
+    });
+  }
+
+  function arrayMinIndex (array) {
+    return array.indexOf(arrayMinValue(array));
+  };
+
+  function countFemale (array) {
+    return array.reduce(function (p, c) {
+      return c.gender === 'F' ? p + 1 : p;
+    }, 0);
+  };
+
+  function FMratio (female, size) {
+    return female * 100 / size;
+  };
+
+  function isFemale (person) {
+    if (person.gender === 'F') {
+      return true;
+    }
+    return false;
+  }
+
+  // test functions
+  function randomGender () {
+    return Math.random() > 0.5 ? 'F' : 'M';
+  };
+
+  var testList= function (n) {
+    var array = [];
+    for (var i = 0; i < n; i += 1) {
+      array.push({id: i, name: i, gender: randomGender()});
+    }
+    return array;
+  };
+
+  // score functions
+  function scoreFrequency (person, group, frequency) {
+    var score = 0;
+    var i = 0;
+    for (i = 0; i < group.length; i += 1) {
+      score += frequency[person.id][group[i].id] +
+      frequency[group[i].id][person.id];
+    }
+    return score;
+  };
+
+  function scoreSize (size, groupSize, shifts) {
+    if ((size + 1) > groupSize) {
+      return size * shifts + 1;
+    }
+    return 0;
+  };
+
+  function scoreRatio (person, group, people, groupSize) {
+    // the ratio of at least two from same gender
+    var twoRatio = 2 * 100 / groupSize;
+
+    var female = countFemale(gorup);
+    var currentFMratio = FMratio(female, group.length);
+
+    female += isFemale(person) ? 1 : 0;
+    var newFMratio = FMratio(female, group.length + 1);
+
+    female = countFemale(people);
+    var globalFMratio = FMratio(female, people.length);
+
+    var currentFMDiffRatio = Math.abs(globalFMratio - currentFMratio);
+    var newFMDiffRatio = Math.abs(globalFMratio - newFMDiffRatio);
+
+    if (// ratio not getting better
+        newFMDiffRatio > currentFMDiffRatio ||
+        // only one gender so far
+        (newFMDiffRatio === currentFMDiffRatio === 0) ||
+        // at least two of each
+        newFMRatio < twoRatio) {
+      return newFMDiffRatio * (group.size + 1) / 100;
+    }
+    return 0;
+  };
 
 }
